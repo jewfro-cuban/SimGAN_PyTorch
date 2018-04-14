a34d6451f9da143b18e1b6dc3b6bb70fbff9fe54
automan <273918227@qq.com>
2018-04-14 16:53:12 +0800

    speed up

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import torch.utils.data as Data
 import torchvision
 from VisdomPortal.visportal.core import VisdomPortal
 from torch import nn
+import numpy as np
 from torch.autograd import Variable
 from torchvision import transforms
 from helper_func.image_history_buffer import ImageHistoryBuffer
@@ -11,7 +12,40 @@ from helper_func.image_utils import generate_img_batch, get_accuracy
 import config as cfg
 import os
 
+import time
+
 vis = VisdomPortal(env_name='SimGAN_{}'.format('Eye'))
+
+
+def loop_iter(dataloader):
+    while True:
+        for data in iter(dataloader):
+            yield data
+
+
+class MyTimer():
+    def __init__(self):
+        self.time_dict = {}
+        self.count_dict = {}
+        self.t0 = 0.
+
+    def track(self):
+        self.t0 = time.time()
+
+    def add_value(self, title):
+        value = time.time() - self.t0
+        self.t0 = 0.
+
+        if not title in self.time_dict:
+            self.time_dict[title] = value
+            self.count_dict[title] = 1
+        else:
+            self.time_dict[title] = (self.time_dict[title] * self.count_dict[title] + value)
+            self.count_dict[title] += 1
+            self.time_dict[title] /= self.count_dict[title]
+
+    def get_all_time(self):
+        return self.time_dict
 
 
 class Main(object):
@@ -25,10 +59,12 @@ class Main(object):
         self.self_regularization_loss = None
         self.local_adversarial_loss = None
         self.delta = None
-
+        self.my_timer = MyTimer()
         # data
         self.fake_images_loader = None
         self.real_images_loader = None
+        self.fake_images_iter = None
+        self.real_images_iter = None
         self.current_step = 0
 
     def build_network(self):
@@ -85,19 +121,24 @@ class Main(object):
             transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
         fake_folder = torchvision.datasets.ImageFolder(root=cfg.syn_path, transform=transform)
+
         self.fake_images_loader = Data.DataLoader(fake_folder, batch_size=cfg.batch_size, shuffle=True,
-                                                  pin_memory=False, drop_last=True, num_workers=0)
+                                                  pin_memory=False, drop_last=True, num_workers=3)
+
         real_folder = torchvision.datasets.ImageFolder(root=cfg.real_path, transform=transform)
         self.real_images_loader = Data.DataLoader(real_folder, batch_size=cfg.batch_size, shuffle=True,
-                                                  pin_memory=False, drop_last=True, num_workers=0)
+                                                  pin_memory=False, drop_last=True, num_workers=3)
+        self.fake_images_iter = loop_iter(self.fake_images_loader)
+        self.real_images_iter = loop_iter(self.real_images_loader)
 
 
     def pretrain_generator(self):
         # we first train the Rθ network with just self-regularization loss for 1,000 steps
         print('pre-training the refiner network %d times...' % cfg.g_pretrain)
         self.G.train()
+        # fake_iter = iter(self.fake_images_loader)
         for step in range(cfg.g_pretrain):
-            faked_images, _ = self.fake_images_loader.__iter__().next()
+            faked_images, _ = next(self.fake_images_iter)
             faked_images = Variable(faked_images).cuda(cfg.cuda_num)
             refined_images = self.G(faked_images)
             # regularization loss
@@ -115,13 +156,15 @@ class Main(object):
 
     def pretrain_discrimintor(self):
         # and Dφ for 200 steps (one mini-batch for refined images, another for real)
-        print('pre-training the discriminator network %d times...' % cfg.g_pretrain)
+        print('pre-training the discriminator network %d times...' % cfg.d_pretrain)
         self.D.train()
         self.G.eval()
         for step in range(cfg.d_pretrain):
-            real_images, _ = self.real_images_loader.__iter__().next()
+            real_images, _ = next(self.real_images_iter)
+            # real_images, _ = self.real_images_loader.__iter__().next()
             real_images = Variable(real_images).cuda(cfg.cuda_num)
-            fake_images, _ = self.fake_images_loader.__iter__().next()
+            fake_images, _ = next(self.fake_images_iter)
+            # fake_images, _ = self.fake_images_loader.__iter__().next()
             fake_images = Variable(fake_images).cuda(cfg.cuda_num)
             # refine fake images
             refined_images = self.G(fake_images)
@@ -159,83 +202,105 @@ class Main(object):
         assert self.current_step < cfg.train_steps, 'Target step is smaller than current step!'
 
         for step in range(self.current_step, cfg.train_steps):
-            print('Step[%d/%d]' % (step, cfg.train_steps))
             self.current_step = step
             self.D.train()
             self.G.train()
 
             for index in range(cfg.k_g*2):
-                fake_images, _ = self.fake_images_loader.__iter__().next()
-                # forward
+                self.my_timer.track()
+                fake_images, _ = next(self.fake_images_iter)
                 fake_images = Variable(fake_images).cuda(cfg.cuda_num)
+                self.my_timer.add_value('get_fake_images')
+                # forward #1
+                self.my_timer.track()
                 refined_images = self.G(fake_images)
+                self.my_timer.add_value('refine_fake')
+                # forward #2
+                self.my_timer.track()
                 refined_predictions = self.D(refined_images).view(-1, 2)
-                refined_labels = Variable(torch.zeros(refined_predictions.size(0)).type(torch.LongTensor)).cuda(cfg.cuda_num)
+                self.my_timer.add_value('predict_fake')
                 # calculate loss
+                self.my_timer.track()
+                refined_labels = Variable(torch.zeros(refined_predictions.size(0)).type(torch.LongTensor)).cuda(cfg.cuda_num)
                 reg_loss = self.self_regularization_loss(refined_images, fake_images)
                 reg_loss = torch.mul(reg_loss, self.delta)
                 adv_loss = self.local_adversarial_loss(refined_predictions, refined_labels)
                 r_loss = reg_loss + adv_loss
-                # update
+                self.my_timer.add_value('refine_loss')
+                # backward
+                self.my_timer.track()
                 self.combined_optimizer.zero_grad()
                 r_loss.backward()
                 self.combined_optimizer.step()
-
-            print('(Refiner) loss:%.4f reg_loss:%.4f, adv_loss:%.4f' % (r_loss.data[0], reg_loss.data[0], adv_loss.data[0]))
+                self.my_timer.add_value('refine_backward')
 
             # ========= train the D =========
             self.G.eval()
             self.D.train()
 
             for index in range(cfg.k_d):
-                # generate refined images
-                fake_images, _ = self.fake_images_loader.__iter__().next()
+                # get images
+                self.my_timer.track()
+                fake_images, _ = next(self.fake_images_iter)
                 fake_images = Variable(fake_images).cuda(cfg.cuda_num)
-                refined_images = self.G(fake_images)
-                refined_images = refined_images.detach()
-                # real images
-                real_images, _ = self.real_images_loader.__iter__().next()
+                real_images, _ = next(self.real_images_iter)
                 real_images = Variable(real_images).cuda(cfg.cuda_num)
-                # visualization
-                vis.draw_images(real_images, 'Real Images')
-                vis.draw_images(fake_images, 'Simulated Images')
-                vis.draw_images(refined_images, 'Refined Images')
-
+                self.my_timer.add_value('get_all_images')
+                # generate refined images
+                self.my_timer.track()
+                refined_images = self.G(fake_images)
+                self.my_timer.add_value('refine_fake')
                 # use a history of refined images
+                self.my_timer.track()
+                refined_images = refined_images.detach()
                 half_batch_from_image_history = image_history_buffer.get_from_image_history_buffer()
                 image_history_buffer.add_to_image_history_buffer(refined_images.cpu().data.numpy())
-
                 if len(half_batch_from_image_history):
                     history_refined_images = torch.from_numpy(half_batch_from_image_history)
                     history_refined_images = Variable(history_refined_images).cuda(cfg.cuda_num)
                     refined_images[:cfg.batch_size // 2] = history_refined_images
-
-                # predict real images
+                self.my_timer.add_value('get_history_im')
+                # predict images
+                self.my_timer.track()
                 real_predictions = self.D(real_images).view(-1, 2)
+                refined_predictions = self.D(refined_images).view(-1, 2)
+                self.my_timer.add_value('predict_all')
+                # get all loss
+                self.my_timer.track()
                 real_labels = Variable(torch.zeros(real_predictions.size(0)).type(torch.LongTensor)).cuda(
                     cfg.cuda_num)
                 pred_loss_real = self.local_adversarial_loss(real_predictions, real_labels)
                 acc_real = get_accuracy(real_predictions, 'real')
-                # predict refined images
-                refined_predictions = self.D(refined_images).view(-1, 2)
                 refined_labels = Variable(torch.ones(refined_predictions.size(0)).type(torch.LongTensor)).cuda(
                     cfg.cuda_num)
                 pred_loss_refn = self.local_adversarial_loss(refined_predictions, refined_labels)
                 acc_ref = get_accuracy(refined_predictions, 'refine')
                 d_loss = pred_loss_refn + pred_loss_real
-
+                self.my_timer.add_value('combine_loss')
+                self.my_timer.track()
                 self.D.zero_grad()
                 d_loss.backward()
                 self.discriminator_loss.step()
+                self.my_timer.add_value('combine_backward')
 
             if step % cfg.d_pre_per == 0:
+                print('------Step[%d/%d]------' % (step, cfg.train_steps))
+                print('(Refiner) loss:%.4f reg_loss:%.4f, adv_loss:%.4f' % (
+                r_loss.data[0], reg_loss.data[0], adv_loss.data[0]))
                 print('(Discrimintor) loss:%.4f real:%.4f(%.2f) refined:%.4f(%.2f)'
                       % (d_loss.data[0] / 2, pred_loss_real.data[0], acc_real, pred_loss_refn.data[0], acc_ref))
 
+                # visualization
+                vis.draw_images(real_images, 'Real Images')
+                vis.draw_images(fake_images, 'Simulated Images')
+                vis.draw_images(refined_images, 'Refined Images')
                 vis.draw_curve(value=r_loss, step=step, title='Refiner Loss')
                 vis.draw_curve(value=d_loss, step=step, title='Discriminator Loss')
                 vis.draw_curve(value=acc_real, step=step, title='Real Images Discriminator Accuracy')
                 vis.draw_curve(value=acc_ref, step=step, title='Refined Images Discriminator Accuracy')
+
+                time_dict = self.my_timer.get_all_time()
+                vis.draw_bars(time_dict, 'time_costs')
 
             if step % cfg.save_per == 0 and step > 0:
                 print('Save two model dict.')
